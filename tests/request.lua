d8d99ec5d3aa11e0bcada44c910054c511d7e15e
Norman Clarke <norman@njclarke.com>
2011-04-04 11:22:55 -0300

    Added example assert/shake test comment.

--- tests/request.lua
+++ tests/request.lua
@@ -17,3 +17,16 @@ context('m2-lua', function()
         end)
     end)
 end)
+
+-- Alternatively, without Telescope, this could be written like this:
+--
+-- for line in io.lines('tests/request_payloads.txt') do
+--     local req, err = request.parse(line)
+--     print(line)
+--     assert(not err)
+--     assert(req)
+-- end
+--
+-- If you run this with plain Lua, it will exit in the first failed
+-- assertion. If you run it with Shake, then it will run all the
+-- tests and report the successes and failures.
